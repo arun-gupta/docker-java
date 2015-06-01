6c39af3c2fa023cd60c13dccdb6b6ea452b0ccf5
arun-gupta <arun.gupta@gmail.com>
2015-06-01 14:23:53 -0700

    adding a script to expose ports

---
+++ instructor/virtualbox-ports.sh
@@ -0,0 +1,10 @@
+#Open Registry Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port5000,tcp,,5000,,5000";
+#Open Nexus Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port8081,tcp,,8081,,8081";
+#Open HTTP Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port8082,tcp,,8082,,8082";
+#Open gitlab ports
+VBoxManage controlvm "mymachine" natpf1 "tcp-port10022,tcp,,10022,,10022";
+VBoxManage controlvm "mymachine" natpf1 "tcp-port10080,tcp,,10080,,10080";
+
