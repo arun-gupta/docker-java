da40e4fd0095293d903326219169b77fe2e846aa
arun-gupta <arun.gupta@gmail.com>
2015-06-02 10:42:50 -0700

    adding a script to push images to local registry

---
+++ instructor/push-images-to-registry.sh
@@ -0,0 +1,27 @@
+# Wildfly
+docker pull jboss/wildfly
+docker tag jboss/wildfly localhost:5000/wildfly
+docker push localhost:5000/wildfly
+
+# Managed WildFly
+docker tag instructor/wildfly-management localhost:5000/wildfly-management
+docker push localhost:5000/wildfly-management
+
+# Ticket-monster+PGSQ+WildFly
+docker tag instructor/ticketmonster-pgsql-wildfly localhost:5000/ticketmonster-pgsql-wildfly
+docker push localhost:5000/ticketmonster-pgsql-wildfly
+
+# Postgres
+docker pull postgres
+docker tag postgres localhost:5000/postgres
+docker push localhost:5000/postgres
+
+# Modcluster
+docker pull goldmann/mod_cluster
+docker tag goldmann/mod_cluster localhost:5000/mod_cluster
+docker push localhost:5000/mod_cluster
+
+#Java EE 7 hol
+docker pull arungupta/javaee7-hol
+docker tag arungupta/javaee7-hol localhost:5000/javaee7-hol
+docker push localhost:5000/javaee7-hol
