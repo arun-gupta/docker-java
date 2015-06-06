5b77a9c25f24cc1cf4366155866c310cec8d75ff
arun-gupta <arun.gupta@gmail.com>
2015-06-05 18:32:49 -0700

    adding scripts to create/delete k8s resources

---
+++ attendees/kubernetes/create-script.sh
@@ -0,0 +1,3 @@
+./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/mysql-service.yaml 
+./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/mysql.yaml 
+./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/wildfly.yaml 
---
+++ attendees/kubernetes/delete-script.sh
@@ -0,0 +1,3 @@
+./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/wildfly.yaml 
+./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/mysql.yaml 
+./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/mysql-service.yaml 
