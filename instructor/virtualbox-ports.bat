6016f2a908867b4a63afee4a7fa85b6bcf0b3bf5
Markus Eisele <myfear@web.de>
2015-06-10 10:29:27 +0200

    Windows Version
    of virtualbox-ports.sh / .bat

---
+++ instructor/virtualbox-ports.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Open Registry Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port5000,tcp,,5000,,5000";
+echo Open Nexus Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port8081,tcp,,8081,,8081";
+echo Open HTTP Server port
+VBoxManage controlvm "mymachine" natpf1 "tcp-port8082,tcp,,8082,,8082";
+echo Open gitlab ports
+VBoxManage controlvm "mymachine" natpf1 "tcp-port10022,tcp,,10022,,10022";
+VBoxManage controlvm "mymachine" natpf1 "tcp-port10080,tcp,,10080,,10080";
+
