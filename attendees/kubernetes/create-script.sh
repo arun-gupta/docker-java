7af4b310793849cb5c86759e7d49f1372861615e
Arun Gupta <arun.gupta@gmail.com>
2016-09-28 07:47:12 -0700

    refactoring files to updated repos

--- attendees/kubernetes/create-script.sh
+++
@@ -1,3 +0,0 @@
-./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/app-mysql-pod.yaml
-./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/app-mysql-service.yaml
-./cluster/kubectl.sh --v=5 create -f ../../attendees/kubernetes/app-wildfly-rc.yaml
--- attendees/kubernetes/delete-script.sh
+++
@@ -1,3 +0,0 @@
-./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/app-wildfly-rc.yaml
-./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/app-mysql-service.yaml
-./cluster/kubectl.sh --v=5 delete -f ../../attendees/kubernetes/app-mysql-pod.yaml
